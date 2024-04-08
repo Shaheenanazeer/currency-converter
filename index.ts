061774f71969d9cc3ae7a92afcfdd715cc5a0e95
Shaheena Nazeer <Shaheenanazeer2@gmail.com>
2024-04-07 17:57:35 -0700

    currency converter application setup with basic functionality.

---
+++ index.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(("\n \t Welcome to currency converter\n"));
+
+// Define the list of currencies and thier exchange rates
+let exchange_rate: any = {
+   "USD" : 1,              
+   "EUR" : 0.85,
+   "JYP" : 113,
+   "CAD" : 1.3,
+   "AUD" : 1.65,
+   "PKR" : 277.70,
+
+}
+// PROMT THE USER TO SELECT CURRENCIES TO CONVERT FROM AND TO
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "select the currency to convert from:",
+        choices: ["USD","EUR", "JYP", "CAD", "AUD", "PKR"]
+        
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "select the currency to convert into:",
+        choices: ["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+   {
+     
+    name: "amount",
+     type: "input",
+     message: "Enter the amount to convert:",
+   }
+
+]);
+// perform currency conversion by using formula
+
+ let from_amount =  exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+
+// FORMULA OF CONVERSION
+
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+// DISPLAY  THE CONVERTED AMOUNT
+
+console.log(`converted Amount = ${chalk.bold(converted_amount)}`);
